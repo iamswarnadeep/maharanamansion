27aa8c2032324daba7152bace6f2dc359fa911a1
Prasarnet <mail2swarnadeep@gmail.com>
2025-08-16 20:50:48 +0530

    ggghfh

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import ImageSlider from "@/components/ImageSlider";
+import VideoHero from "@/components/VideoHero";
 import About from "@/components/About";
 import Amenities from "@/components/Amenities";
 import Contact from "@/components/Contact";
@@ -10,7 +11,7 @@ const Index = () => {
     <div className="min-h-screen">
       <Header />
       <section id="home">
-        <ImageSlider />
+        <VideoHero />
       </section>
       <About />
       <Amenities />
