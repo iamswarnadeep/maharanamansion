c899d751fa6920f5b248ca551f93f0eb8d2fc94c
Prasarnet <mail2swarnadeep@gmail.com>
2026-05-23 14:14:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // <-- Add this here to fix the blank screen on cPanel
   server: {
     host: "::",
     port: 8080,
